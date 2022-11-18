4993c3a05b97349b5c8cd31f98b5a5787b2b094e
Ian Myers <imyers236@yahoo.com>
2022-11-17 16:05:02 -0800

    Hw6

--- hw6_1.s
+++ hw6_1.s
@@ -1,28 +1,77 @@
 # ----------------------------------------
 #   File: hw6_1.s
-#   Description: ...
+#   Description:flips 2 words to be the other way around
+#
 # ----------------------------------------
 
     .global _start
     .text
 
 _start: 
-        #... write our code here ...
-        
+                #... write our code here ...
+                movq    $str, %rbx  # moves the pointer of the first value into rbx
+                movb    (%rbx), %dl #puts the value at the pointer 
+                movb    $32, %cl #moves blank space into rcx
+                cmpb    %cl, %dl # compares the string bit to space
+                jne     findSpace # if not equal it loops
+                jmp     mainRotate # if equal it moves on
 
-        #write to stdout
-        movq    $1, %rax
-        movq    $1, %rdi
-        movq    $str, %rsi
-        movq    $len, %rdx
-        syscall
 
-        #exit the program
-        movq    $60, %rax
-        xorq    %rdi, %rdi
-        syscall
 
-        .data
-str:    .ascii "helloworld\n"   #any string here
-        .equ len, (. - str)
+findSpace:      addq    $1, %rbx # adds one to pointer
+                addq    $1, %r8  # adds one to index
+                movb    (%rbx), %dl #puts the value at the pointer 
+                cmpb    %cl, %dl # compares the string bit to space
+                jne     findSpace # if not equal it loops
+                jmp     moveSpace # once space is found jump to move space
+
+moveSpace:      movq    $str, %rcx # moves the pointer of the first value into rcx
+                movb    (%rbx), %dl # moves the space into dl
+rotate1:        subq    $1, %rbx # traverses through the string backwards
+                movb    (%rbx), %al # moves the value into al
+                movb    %al, 1(%rbx) # moves al into next address
+                cmpq    %rbx, %rcx  # sees whether rbx is at the beginning
+                jne     rotate1 # if not repeat
+                movb    %dl, (%rbx) # puts space at the beginning
+
+                movq    %r8, %r9
+                movb    $10, %r10b
+wordLength:     addq    $1, %r9
+                movb    (%rbx,%r9), %dl 
+                cmpb    %r10b, %dl # compares the string bit to space
+                jne     wordLength # if not equal it loops 
+                subq    %r8, %r9
+                subq    $1, %r9
+                jmp     mainRotate
+
+mainRotate:     movq    %r8, %r12
+                addq    %r9, %r12
+repeat:         movq    %r12, %r11
+                movb    (%rbx,%r11), %dl # moves the space into dl
+rotate2:        subq    $1, %r11 # traverses through the string backwards
+                movb    (%rbx,%r11), %al # moves the value into al
+                movb    %al, 1(%rbx,%r11) # moves al into next address
+                cmpq    $0, %r11  # sees whether rbx is at the beginning
+                jne     rotate2 # if not repeat
+                movb    %dl, (%rbx) # puts space at the beginning
+                subq    $1, %r9
+                cmpq    $0, %r9
+                jne     repeat
+
+
+                #write to stdout
+                movq    $1, %rax
+                movq    $1, %rdi
+                movq    $str, %rsi
+                movq    $len, %rdx
+                syscall
+
+                #exit the program
+                movq    $60, %rax
+                xorq    %rdi, %rdi
+                syscall
+
+                .data
+str:            .ascii "helicopter airplane\n"   #any string here
+                .equ len, (. - str)
         
--- hw6_2.s
+++ hw6_2.s
@@ -1,28 +1,97 @@
 # ----------------------------------------
 #   File: hw6_2.s
-#   Description: ...
+#   Description: Uses selection sort and the stack to sort a string of 10 digits
 # ----------------------------------------
 
     .global _start
     .text
 
-_start: 
-        #... write our code here ...
+_start: jmp print1
+
+
+begin:  movq    $str, %rbx  # moves the pointer of the first value into rbx
+        movl    $10, %edx   # index
+        movq    $10, %r10   # index for inner loop
+        movq    $10, %r11    # index for outer loop
+loop1:  movl    %r13d, %ecx
+        cmpl    %edx, %ecx  # pushes all numbers to stack
+        jl      input
+        jmp     body        # moves to sort
+
+input:  pushq   (%rbx)      # pushes onto stack then increments
+        incq    %rbx
+        addl    $1, %r13d
+        jmp     loop1
         
 
-        #write to stdout
+body:   movq    %r9, %rcx    # moves outer counter to inter counter so numbers are not repeated
+        movb    (%rsp, %rcx, 8), %al    # moves value to al
+findMin:incq    %rcx         
+        cmpq    %r10, %rcx   # makes sure counter is not out of numbers
+        jge     place
+        movb    (%rsp, %rcx, 8), %r8b   # moves value to r8b
+        cmpb    %al, %r8b    # if r8b is less than al they will switch
+        jl      switch
+        cmpq    %r10, %rcx   # makes sure counter is not out of numbers
+        jl      findMin      # reloops
+        jmp     place        # jumps to place
+
+switch: xchgb   %r8b, %al    # switchs smaller number to al
+        movq    %rcx, %r12   # moves other numbers index to r12
+        cmpq    %r10, %rcx   # makes sure counter is not out of numbers
+        jl      findMin      # reloops
+        jmp     place        # jumps to place
+
+swap:   movb    (%rsp, %r9, 8), %r8b    # moves number about to be replaced to r8b
+        movb    %r8b, (%rsp, %r12, 8)   # moves r8b to saved index
+        jmp     cont         # continues with place
+
+place:  movb    (%rsp, %r9, 8), %r14b     # moves number about to be replace to bl
+        cmpb    %r14b, %al                # if al is not the same value as bl perform swap
+        jne     swap
+cont:   movb    %al, (%rsp, %r9, 8)     # moves value into index
+        incq    %r9                     # increments index
+        cmpq    %r11, %r9               # makes sure index is not out of space
+        jl      body                    # reloops
+
+        movq    $0, %r13
+        movq    $str, %rbx              # puts original string pointer in rbx
+loop3:  movq    %r13, %rcx                # resets ecx
+        cmpl    %edx, %ecx              # pops all number back into the string 
+        jl      output
+        jmp     print2                  # pops number into string     
+        jmp     done                    # jumps to done
+
+output: popq    %r14
+        movb    %r14b, (%rbx)        
+        incq    %rbx                    
+        addq    $1, %r13
+        jmp     loop3
+
+
+
+done:   #exit the program
+        movq    $60, %rax
+        xorq    %rdi, %rdi
+        syscall
+
+print1:   #write to stdout
         movq    $1, %rax
         movq    $1, %rdi
         movq    $str, %rsi
         movq    $len, %rdx
         syscall
+        jmp begin
 
-        #exit the program
-        movq    $60, %rax
-        xorq    %rdi, %rdi
+print2:   #write to stdout
+        movq    $1, %rax
+        movq    $1, %rdi
+        movq    $str, %rsi
+        movq    $len, %rdx
         syscall
+        jmp done
 
         .data
-str:    .ascii "helloworld\n"   #any string here
+str:    .ascii "3920184356\n"
         .equ len, (. - str)
         
